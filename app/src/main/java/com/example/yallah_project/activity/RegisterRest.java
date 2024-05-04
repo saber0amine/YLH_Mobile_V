828e2c42bb45c35a913e3d74c02e7f8ba97dd59d
saber0amine <120688951+saber0amine@users.noreply.github.com>
2024-05-04 04:05:18 +0000

    Changing to full REST

--- app/src/main/java/com/example/yallah_project/activity/RegisterRest.java
+++ app/src/main/java/com/example/yallah_project/activity/RegisterRest.java
@@ -8,6 +8,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.yallah_project.apis.ApiService;
 import com.example.yallah_project.model.User;
@@ -19,21 +21,21 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import com.example.yallah_project.R;
+import com.example.yallah_project.viewmodel.UserViewModel;
 
 import java.io.IOException;
 
 // Updated RegisterActivity using Retrofit
     public class RegisterRest extends AppCompatActivity implements View.OnClickListener {
 
-        private ApiService apiService;
     private EditText registerName ;
     private EditText registerEmail ;
     private EditText registerPassword ;
 private TextView registerMessage ;
     private Button registerButton ;
     private Button loginbutton ;
-    private static final String BASE_URL = "http://192.168.1.142:8080";
 
+    private UserViewModel userViewModel ;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -46,56 +48,28 @@ private TextView registerMessage ;
              registerName =findViewById(R.id.registerName) ;
             registerEmail = findViewById(R.id.registerEmail);
             registerPassword = findViewById(R.id.registerPassword);
+            userViewModel = new ViewModelProvider(this).get(UserViewModel.class) ;
 
-            // Create Retrofit instance
-            Retrofit retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory
-                            .create())
-                    .build();
-
-            // Create ApiService instance
-            apiService = retrofit.create(ApiService.class);
-
-         }
+             }
 
         @Override
         public void onClick(View v) {
-            // Handle button clicks and registration process
 
-            // Example:
+
             if (v.getId() == R.id.registerButton2) {
-                // Create UserRegistrationRequest object with user input
-                User request = new User();
-                request.setName(registerName.getText().toString());
-                request.setEmail(registerEmail.getText().toString());
-                request.setPassword(registerPassword.getText().toString());
-
-                // Make network request using Retrofit
-                Call<ResponseBody> call = apiService.registerUser(request);
-                call.enqueue(new Callback<ResponseBody>() {
-                    @Override
-                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                        // Handle response from server
-                        if (response.isSuccessful()) {
-                            String message = null;
-                            try {
-                                message = response.body().string();
-                            } catch (IOException e) {
-                                throw new RuntimeException(e);
-                            }
-                            registerMessage.setText(message);
-                        } else {
-                            // Handle error response
-                        }
+                User user = new User();
+                user.setName(registerName.getText().toString());
+                user.setEmail(registerEmail.getText().toString());
+                user.setPassword(registerPassword.getText().toString());
+                LiveData<Boolean> serverRep = userViewModel.insertUser(user);
+                serverRep.observe(this , response -> {
+                    if(serverRep.getValue() != null && serverRep.getValue() == true){
+                        registerMessage.setText("User Registered Successfully");
+                    } else {
+                        registerMessage.setText("User Registration Failed");
                     }
-
-                    @Override
-                    public void onFailure(Call<ResponseBody> call, Throwable t) {
-                        registerMessage.setText("Failed to register user");
-
-                    }
-                });
+                }
+                );
             }
         }
     }
--- app/src/main/java/com/example/yallah_project/viewmodel/UserViewModel.java
+++ app/src/main/java/com/example/yallah_project/viewmodel/UserViewModel.java
@@ -4,21 +4,37 @@ import android.app.Application;
 
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 
+import com.example.yallah_project.apis.ApiService;
 import com.example.yallah_project.model.User;
+import com.example.yallah_project.network.RetrofitClient;
 import com.example.yallah_project.repository.UserRepository;
 
+import java.io.IOException;
 import java.util.List;
 
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class UserViewModel extends AndroidViewModel {
+
+    private ApiService apiService;
     private UserRepository userRepository;
     private LiveData<List<User>> allUsers;
+
+    private int statusCode ;
 
     public UserViewModel(Application application) {
         super(application);
         userRepository = new UserRepository(application);
         allUsers = userRepository.getAllUsers();
+        apiService = RetrofitClient.getClient().create(ApiService.class);
+
     }
+
 
     public LiveData<List<User>> getAllUsers() {
         return allUsers;
@@ -37,4 +53,42 @@ public LiveData<User> getUserByEmailAndPassword(String email, String password) {
     public void update(User user) {
         userRepository.update(user);
     }
+
+
+    // Retrofit Actions
+
+    public LiveData<Boolean> insertUser(User user) {
+
+        MutableLiveData<Boolean> serverResponse = new MutableLiveData<>();
+
+        Call<ResponseBody> call = apiService.registerUser(user);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+
+                // Handle response from server
+                if (response.isSuccessful()) {
+                    serverResponse.setValue(true);
+                } else {
+                    serverResponse.setValue(false);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                // Handle network error
+                t.printStackTrace();
+            }
+        });
+
+
+        return serverResponse;
+    }
+
+
+
+
+
+
+
 }
